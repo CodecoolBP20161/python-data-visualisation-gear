f3a9195a211464a9debd90f374e08bd980cc50c2
laszlo-milu <hockeylaci@gmail.com>
2016-07-13 12:13:52 +0200

    deleted currency_conversion.py
    it's functions are moved to common.py

--- currency_conversion.py
+++
@@ -1,9 +0,0 @@
-# converts all currencies to eur
-def currency_conversion(curr):
-    if curr == 'GBP':
-        x = float(1.19)
-    elif curr == 'USD':
-        x = float(0.91)
-    else:
-        x = 1
-    return x
